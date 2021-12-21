dd58329a4835b0a649098ccd9f2e2b9c4c6cf4ae
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-21 02:48:25 +0100

    Create model.tsx

---
+++ src/components/questionsCards/model.tsx
@@ -0,0 +1,15 @@
+import {AnswerObject} from '../../App'
+
+export type Props = {
+  question: string,
+  answers: string[],
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void,
+  userAnswer: AnswerObject | undefined,
+  questionNumber: number,
+  totalQuestions: number
+}
+
+export type ButtonWrapperProps = {
+  correct: boolean;
+  userClicked: boolean
+}
