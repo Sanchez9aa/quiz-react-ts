3b586532eca5012cfff3ee87085f955af41719ac
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-21 01:04:07 +0100

    Create questionsCards.style.js

---
+++ src/components/questionsCards/questionsCards.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+`
+
+export const StyledNumberQuestion = styled.p`
+
+`
+
+export const StyledWrapperAnswers = styled.div`
+`
+
+export const StyledAnswers = styled.div`
+`
+
+export const StyledAnswerSelected = styled.button``
+
