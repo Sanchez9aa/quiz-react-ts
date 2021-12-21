08fdf822738ce6890f85c642c05d92ee9f351d29
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-21 01:04:04 +0100

    Create app.style.js

---
+++ src/app.style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+
+`
+
+export const StyledTitle = styled.h1`
+
+`
+
+export const StyledButton = styled.button`
+
+`
+
+export const StyledScore = styled.p`
+
+`
+
+export const StyledSelect = styled.select`
+
+`
+
+export const StyledOption = styled.option`
+
+`
